f2e22f2acf28104a02fa86225db75b1a1e452f01
aroy96 <anthony.roy@stonybrook.edu>
2017-03-27 14:52:10 -0400

    Upgraded toolset and erased some files.

--- Balanced/Balanced/assets/scripts/States/GameStateManager.js
+++ Balanced/Balanced/assets/scripts/States/GameStateManager.js
@@ -14,22 +14,28 @@ var States;
         GameStateManager.prototype.setState = function (s, start) {
             if (start === void 0) { start = false; }
             if (s === undefined || s === null) {
+                //TODO: ERROUT
             }
             if (this.state === undefined || this.state === null) {
+                //TODO: ERROUT
             }
             if (this.state.end() == false) {
+                //TODO: ERROUT
             }
             s.init();
             if (start && this.state.startup() == false) {
+                //TODO: ERROUT
             }
             this.state = s;
         };
         GameStateManager.prototype.startState = function () {
             if (this.state.startup() == false) {
+                //TODO: ERROUT
             }
         };
         GameStateManager.prototype.endState = function () {
             if (this.state.end() == false) {
+                //TODO: ERROUT
             }
         };
         GameStateManager.prototype.update = function () {
